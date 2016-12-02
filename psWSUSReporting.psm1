c33c929b72272f086d18ec922c281812bb863b00
joeypiccola <joey.piccola@gmail.com>
2016-12-02 11:56:34 -0700

    downstream support

--- psWSUSReporting.psm1
+++ psWSUSReporting.psm1
@@ -364,7 +364,7 @@ Get all the members from the group 'Contoso Exchange Servers'.
 
 .NOTES
 Author: Joey Piccola
-Last Modified: 06/24/16
+Last Modified: 12/02/16
 #>
 
     [CmdletBinding()]
@@ -395,19 +395,43 @@ Last Modified: 06/24/16
             break
         }
         
+        Write-Verbose "Pulling members from upstream server: $($script.wsus.fulldomainname)"
         $GetUpdateGroup = $WSUS.GetComputerTargetGroups() | Where {$_.name -eq $Name}
         if ($GetUpdateGroup)
         {
             $UpdateGroupMembers = $wsus.getComputerTargetGroup($GetUpdateGroup.Id).GetComputerTargets()
+            Write-Verbose "Found $($UpdateGroupMembers.count)x"
             foreach ($UpdateGroupMember in $UpdateGroupMembers)
             {   
                 Write-output $UpdateGroupMember
+            }
+
+            # go and connect to the downstream server and write out group members from that group as well. this is a mess
+            $downstreamServers = $wsus.GetDownstreamServers()
+            if ($downstreamServers)
+            {
+                foreach ($downstreamServer in $downstreamServers)
+                {
+                    Write-Verbose "Connecting to downstream server $($downstreamServer.fulldomainname)"
+                    # connect to the downstream server
+                    $dsConnection = $null
+                    $dsConnection = [microsoft.updateservices.administration.adminproxy]::getupdateserver($downstreamServer.fulldomainname,$false,8530)
+                    # get the computer from the same group from the downstream
+                    $GetUpdateGroup = $null
+                    $GetUpdateGroup = $dsConnection.GetComputerTargetGroups() | Where {$_.name -eq $Name}
+                    $UpdateGroupMembers = $dsConnection.getComputerTargetGroup($GetUpdateGroup.Id).GetComputerTargets()
+                    Write-Verbose "Pulling members from downstream server: $($dsConnection.name)"
+                    foreach ($UpdateGroupMember in $UpdateGroupMembers)
+                    {   
+                        Write-output $UpdateGroupMember
+                    }
+                }
             }
         }
         else
         {
             Write-Warning "Failed to find group $Name in the WSUS database $($script:wsus.Name)."
-        }                      
+        }                       
     }
     end {}
 }
